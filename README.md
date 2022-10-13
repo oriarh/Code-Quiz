279d0cdf2104c7a028c37a8c6d741e2981b08e44
oriarh <111208485+oriarh@users.noreply.github.com>
2022-10-12 17:12:36 -0700

    Pushing up the final code

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Code-Quiz
-A basic coding web application that allows candidates to prepare for interview quizes
+
+## Descriptions 
+This quiz web application allows candidates to prepare for interview quizes.
+It contains MCQs and is timed. 
+
+## URL
+https://oriarh.github.io/Code-Quiz/
+
+## Screenshot
+!["Image Screenshot"](./screeshot.png)
+
+## Installation
+N/A
+
+## Usage
+When you open the URL provided above, it takes you to the main page where you get the instructions and the a button to start the quiz.
+
+When the button is clicked, it takes you to the questions and the timer starts. 
+
+Every wrong answer leads to a 10 second panelty from the timer.
+
+When the timer goes to zero, the quiz ends. It also tells you if the answer selected by you was correct or wrong.
+
+This web app was built using HTML and CSS & Javascript.
+
+## Credits
+UC Berkeley Extension Instructional Staff
+
+## License
+MIT License
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var startquiz = document.getElementById("startquiz");
 var startseconds = 75;
 var countdowntimer = document.getElementById("countdown");
-var questions = document.getElementById("instructions");
+const questions = document.getElementById("instructions");
+var globalQuestionCounter = 0;
+var result = document.getElementById("result");
 var scores = [];
 
 //Defined objects for each question with keys (question, answer & correct answer)
@@ -63,32 +65,71 @@ const question5 = {
 
 const questionList = [question1, question2, question3, question4, question5];
 
-console.log(questionList[0].correctAnswer);
-
 //assigning code to the start quiz button
 startquiz.addEventListener("click", function () {
     startthetimer();
-    console.log(startseconds);
-    // forquestion1();
     forQuestions();
 });
 
 
-var globalQuestionCounter = 0;
 
 //Changes questions in a loop after the quiz has started
-var changeQuestion = document.getElementById("quiz")
 //assigning code to the start quiz button
+var changeQuestion = document.getElementById("quiz")
 changeQuestion.addEventListener("click", function (event) {
-    if(event.target.nodeName === "BUTTON") {
+    if (event.target.textContent === "Submit") {
+        questions.textContent = "High Scores";
+        result.style.display = "none";
+        document.getElementById("initialssubmit").style.display = "none";
+        var inputinitialss = document.getElementById("styleforquestions");
+        inputinitialss.style.backgroundColor = "lightgray";
+        inputinitialss.readOnly = true;
+        let optionButtons = document.createElement("button");
+        optionButtons.textContent = "Go back";
+        optionButtons.setAttribute("type", "button");
+        optionButtons.setAttribute("class", "options");
+        optionButtons.setAttribute("id","goBack")
+        optionButtons.style.display = "block";
+        optionButtons.style.marginLeft = "191px";
+        optionButtons.style.marginTop = "20px";
+        document.getElementById("quiz").appendChild(optionButtons);
+
+        let Clearhighscores = document.createElement("button");
+        Clearhighscores.textContent = "Clear high scores";
+        Clearhighscores.setAttribute("type", "button");
+        Clearhighscores.setAttribute("class", "options");
+        Clearhighscores.setAttribute("id","clearHighScores")
+        Clearhighscores.style.display = "inline";
+        Clearhighscores.style.marginLeft = "191px";
+        Clearhighscores.style.marginTop = "-1px ";
+        document.getElementById("quiz").appendChild(Clearhighscores);
+    }
+    else if (event.target.textContent === "Go back") {
+        document.getElementById("goBack").addEventListener("click",function(){
+            window.location.reload(true); 
+
+        })}
+
+    else if (event.target.textContent === "Clear high scores") {
+        document.getElementById("clearHighScores").addEventListener("click",function(){
+            alert("When I wrote this code, only me and God knew how it works. Now only God knows.")
+        });
+    }
+
+    else if(event.target.nodeName === "BUTTON") {
         var buttontext = event.target.textContent;
         var activequestion = questionList[globalQuestionCounter-1];
         if(buttontext != activequestion.correctAnswer) {
             startseconds-=10;
+            result.textContent = "Wrong";
+        }
+        else {
+            result.textContent = "Correct!"
         }
         console.log(buttontext,activequestion.correctAnswer);
         forQuestions();
-    };
+        result.style.display = "flex";
+    }
 }
 );   
 
@@ -114,14 +155,13 @@ function forQuestions() {
         codingQuizchallenge.style.marginLeft = "192px";
         codingQuizchallenge.style.display = "block";
         codingQuizchallenge.textContent = "All done!";
-        //var highscore = getItem()Enter keyname here to fetch from local storage));
-        questions.textContent = "Your final score is" + highscore;
         questions.style.marginTop = "30px";
         var answerList = document.getElementById("ol");
         answerList.style.display = "none";
         //Creating the input bar to enter initials
         var inputinitials = document.createElement("input");
         inputinitials.setAttribute("type","text");
+        inputinitials.setAttribute("id","styleforquestions");
         inputinitials.setAttribute("placeholder","Enter initials Here")
         inputinitials.style.marginTop = "27px";
         document.getElementById("quiz").appendChild(inputinitials);
@@ -140,10 +180,12 @@ function forQuestions() {
                 initials: inputinitials.value,
                 score: startseconds,
             })
-            localStorage.setItem("scores",JSON.stringify(scores));
-    
+            localStorage.setItem("scores",JSON.stringify(scores));    
         });
         document.getElementById("quiz").appendChild(optionButtons);
+        showhighscore();
+        var endtime = document.getElementById("time");
+        endtime.style.display = "none";
 
     } else {
         codingQuizchallenge = document.querySelector("#mainHeading");
@@ -154,19 +196,19 @@ function forQuestions() {
         questions.setAttribute("class","styleforquestions")
         var answerList = document.getElementById("ol");
 
+        // if there no existing OL element, then create one.
         if (answerList == null){
             answerList = document.createElement("ol");
             document.getElementById("quiz").appendChild(answerList);
             answerList.setAttribute("id", "ol");
-            console.log("created OL");
+
+        //if there is an existing OL, then remove its child (the option buttons for the previous question)
         } else {
-            console.log("found old ol");
-            while (answerList.firstChild){
+            while (answerList.firstChild) {
                 answerList.removeChild(answerList.firstChild);
-                console.log("removed child");
             }
         }
-        console.log(answerList);
+
         for (i = 1; i < 5; i++) {
             let optionButtons = document.createElement("button");
             optionButtons.textContent = myQuestion.answers[i];
@@ -174,8 +216,17 @@ function forQuestions() {
             optionButtons.setAttribute("class", "options");
             optionButtons.setAttribute("id", "options" + i)
             answerList.appendChild(optionButtons);
-            console.log("appending button" + optionButtons);
         };
     };
 };
 
+function showhighscore() {
+        var highscore = countdowntimer.textContent;
+        var wronghighscore = startseconds;
+        if (result.textContent != "Correct!") {
+            questions.textContent = "Your high score is " + wronghighscore;
+        }
+        else {
+            questions.textContent = "Your high score is " + highscore;
+        };
+}
